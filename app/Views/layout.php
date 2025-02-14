5ba76aec2835d780026342a0eb6207dc24bb249b
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2025-02-14 11:30:52 -0600

    add: favicon

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -32,6 +32,13 @@
     <meta name="copyright" content="© <?= date('Y') ?> Massive Home. Todos los derechos reservados." />
 
 
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+    <link rel="manifest" href="/site.webmanifest" />
+
 
     <link href="<?= base_url('/assets/css/app.css') ?>" rel="stylesheet">
 </head>
