37bd99c675af6e0930fb2a0d129ad7b84b260aa4
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2025-02-20 11:53:12 -0600

    fix: problem with filters for products

--- app/Views/products.php
+++ app/Views/products.php
@@ -5,85 +5,9 @@
 
 <section>
     <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-        <!-- <header>
-            <h2 class="text-xl font-bold text-gray-900 sm:text-3xl">Product Collection</h2>
-
-            <p class="mt-4 max-w-md text-gray-500">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque praesentium cumque iure
-                dicta incidunt est ipsam, officia dolor fugit natus?
-            </p>
-        </header> -->
 
         <form class="mt-8 sm:flex sm:items-center sm:justify-between">
             <div class="block sm:hidden">
-                <details class="group [&_summary::-webkit-details-marker]:hidden">
-                    <summary
-                        class="flex cursor-pointer items-center gap-2 border-b border-gray-400 pb-1 text-gray-900 transition hover:border-gray-600">
-                        <span class="text-sm font-medium"> Categorías </span>
-
-                        <span class="transition group-open:-rotate-180">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                class="size-4">
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                            </svg>
-                        </span>
-                    </summary>
-
-                    <div
-                        class="z-50 group-open:absolute group-open:top-auto group-open:mt-2 ltr:group-open:start-0">
-                        <div class=" w-full rounded-sm border border-gray-200 bg-white">
-                            <header class="flex items-center justify-between p-4">
-                                <span class="text-sm text-gray-700" id="filter_categories_selected_counter"> 0 Selected </span>
-
-                                <div>
-                                    <button type="button" class="text-sm text-gray-900 underline underline-offset-4" onclick="resetCheckboxes()">
-                                        Reset
-                                    </button>
-
-                                    <button type="submit" class="text-sm text-gray-900 underline underline-offset-4">
-                                        Filtrar
-                                    </button>
-                                </div>
-                            </header>
-
-                            <ul class="space-y-1 border-t border-gray-200 p-4">
-
-                                <?php foreach ($categories as $category): ?>
-                                    <?php
-                                    $checked = '';
-                                    if (is_array($selectedCategoryIds)) {
-                                        $checked = in_array($category['id'], $selectedCategoryIds) ? 'checked' : '';
-                                    }
-                                    ?>
-                                    <li>
-                                        <label for="FilterCategory<?= $category['id'] ?>" class="inline-flex items-center gap-2">
-                                            <input
-                                                type="checkbox"
-                                                id="FilterCategory<?= $category['id'] ?>"
-                                                name="category[]"
-                                                value="<?= $category['id'] ?>"
-                                                class="size-5 rounded-sm border-gray-300 checks-categories"
-                                                <?= $checked ?> />
-
-                                            <span class="text-sm font-medium text-gray-700"> <?= $category['categoryName'] ?> </span>
-                                        </label>
-                                    </li>
-                                <?php endforeach; ?>
-
-
-                            </ul>
-                        </div>
-                    </div>
-                </details>
-
                 <!-- <button
                     class="flex cursor-pointer items-center gap-2 border-b border-gray-400 pb-1 text-gray-900 transition hover:border-gray-600">
                     <span class="text-sm font-medium"> Filters & Sorting </span>
@@ -100,7 +24,7 @@
                 </button> -->
             </div>
 
-            <div class="hidden sm:flex sm:gap-4">
+            <div class="flex gap-4">
                 <div class="relative">
                     <details class="group [&_summary::-webkit-details-marker]:hidden">
                         <summary
