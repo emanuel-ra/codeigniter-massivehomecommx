40f4dc13824808c456cf50d98b955c2587bcd0fc
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2025-02-24 15:20:32 -0600

    feat: re-design grid from home page

--- app/Views/sections/home/products.php
+++ app/Views/sections/home/products.php
@@ -1,13 +1,8 @@
 <div class="bg-white">
-    <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+    <div class="mx-auto max-w-2xl px-4 py-8 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 class="sr-only">Products</h2>
 
-        <div class="mb-5">
-            <a href="/products" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700">
-                Ver mas productos
-            </a>
-        </div>
-        <div class="grid grid-cols-2 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        <div class="grid grid-cols-2 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 xl:gap-x-8">
 
             <?php foreach ($products as $index => $item): ?>
                 <?php
@@ -18,12 +13,18 @@
                     <img src="https://massivehome.com.mx/<?= $path ?><?= $img ?>"
                         alt="<?= $item->nameProduct ?>"
                         onerror="this.onerror=null; this.src='https://massivehome.com.mx/images/productos/<?= $item->Img ?>';"
-                        class="aspect-square w-full rounded-lg bg-gray-200 group-hover:opacity-75 ">
+                        class="aspect-square w-full bg-gray-200 group-hover:opacity-75 ">
                     <h3 class="mt-4 text-sm text-gray-700"><?= $item->nameProduct ?></h3>
                     <p class="mt-1 text-lg font-medium text-gray-900"><?= $item->product_code2 ?></p>
                 </a>
             <?php endforeach; ?>
             <!-- More products... -->
+        </div>
+
+        <div class="mb-5 flex justify-center text-center py-4">
+            <a href="/products" class=" text-slate-800 hover:text-green-600 uppercase font-semibold hover:underline ease-in-out duration-150">
+                Ver mas productos
+            </a>
         </div>
 
     </div>
