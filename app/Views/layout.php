1fbce6dd5b43abd9aba98ce8cd052efcf2b8d82b
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2025-02-25 13:09:25 -0600

    Edit: metas

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -7,13 +7,13 @@
     <title>Massive Home</title>
 
     <meta name="title" content="Massive Home - Importador Directo de las Mejores Marcas en Tecnología">
-    <meta name="description" content="Descubre la tienda en línea líder en México, con un amplio catálogo de productos de iluminación, sonido y electrónica. Ofrecemos calidad, variedad y precios competitivos, respaldados por un servicio al cliente excepcional. ¡Tu mejor opción para equipar tu hogar o negocio!">
+    <meta name="description" content="Con más de 20 años de experiencia, diseñamos y desarrollamos soluciones innovadoras para nuestros clientes. Como fabricante líder en China, contamos con una línea de producción que abarca el 95% de los repuestos en nuestra propia fábrica, asegurando calidad y control total en cada etapa del proceso. Nos comprometemos con la satisfacción del cliente, enfocándonos en la última tecnología del mercado para ofrecer productos adecuados y evolucionar junto a nuestros clientes.">
     <meta name="keywords" content="luminaria suburbana, bocinas, bocinas bluetooth, bocinas profesionales, bafle, iluminación solar, iluminación LED, iluminación Guadalajara, bocinas Guadalajara, distribuidores de iluminación, distribuidores de bocinas, audífonos bluetooth, audífonos inalámbricos, manos libres, cargadores, diademas, cámaras de seguridad, cámaras WiFi, focos LED, tubos LED, dispensadores automáticos, soporte para TV, ventiladores, ventiladores de techo, audio profesional, iluminación exterior, gadgets tecnológicos">
     <meta name="robots" content="index, follow">
 
     <!-- Meta Open Graph (Para redes sociales, especialmente Facebook y WhatsApp) -->
     <meta property="og:title" content="Massive Home - Importador Directo de las Mejores Marcas en Tecnología" />
-    <meta property="og:description" content="Encuentra bocinas, iluminación LED, cámaras WiFi, audífonos, cargadores y más al mejor precio. Compra directo del importador." />
+    <meta property="og:description" content="Con más de 20 años de experiencia, diseñamos y desarrollamos soluciones innovadoras para nuestros clientes. Como fabricante líder en China, contamos con una línea de producción que abarca el 95% de los repuestos en nuestra propia fábrica, asegurando calidad y control total en cada etapa del proceso. Nos comprometemos con la satisfacción del cliente, enfocándonos en la última tecnología del mercado para ofrecer productos adecuados y evolucionar junto a nuestros clientes." />
     <meta property="og:image" content="<?= base_url() ?>assets/images/RAMON-CORONA-148.webp" />
     <meta property="og:url" content="<?= base_url() ?>" />
     <meta property="og:type" content="website" />
