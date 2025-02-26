ac050d03aded559f1dd734bf3fba5fc1471d8175
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2025-02-26 14:02:58 -0600

    edit grid of products

--- app/Views/sections/home/products.php
+++ app/Views/sections/home/products.php
@@ -1,9 +1,11 @@
 <div class="bg-white">
-    <div class="mx-auto max-w-2xl px-4 py-4 sm:px-6 sm:py-12 lg:max-w-7xl lg:px-8">
+    <div class="mx-auto mt-2 px-4 ease-in-out duration-150">
+        <!-- max-w-2xl px-4 py-4 sm:px-6 sm:py-12 lg:max-w-7xl lg:px-8 -->
         <h2 class="sr-only">Products</h2>
 
-        <div class="grid grid-cols-2 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 xl:gap-x-8">
+        <div class="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-1 ">
 
+            <!-- -->
             <?php foreach ($products as $index => $item): ?>
                 <?php
                 $img = ($item->ImgWebp == '') ? $item->Img : $item->ImgWebp;
@@ -12,20 +14,12 @@
                 <a href="<?= base_url('/product/' . $item->id) ?>" class="group">
                     <img src="https://massivehome.com.mx/<?= $path ?><?= $img ?>"
                         alt="<?= $item->nameProduct ?>"
-                        onerror="this.onerror=null; this.src='https://massivehome.com.mx/images/productos/<?= $item->Img ?>';"
-                        class="aspect-square w-full bg-gray-200 group-hover:opacity-75 ">
-                    <h3 class="mt-4 text-sm text-gray-700"><?= $item->nameProduct ?></h3>
-                    <p class="mt-1 text-lg font-medium text-gray-900"><?= $item->product_code2 ?></p>
+                        class="w-full h-auto max-w-full rounded shadow "
+                        onerror="this.onerror=null; this.src='https://massivehome.com.mx/images/productos/<?= $item->Img ?>';">
+                    <!-- <h3 class="mt-4 text-sm text-gray-700"><?= $item->nameProduct ?></h3>
+                    <p class="mt-1 text-lg font-medium text-gray-900"><?= $item->product_code2 ?></p> -->
                 </a>
             <?php endforeach; ?>
-            <!-- More products... -->
         </div>
-
-        <div class="mb-2 flex justify-center text-center py-4" aria-label="Ver más productos">
-            <a href="/products" class=" text-slate-800 hover:text-green-600 uppercase font-semibold hover:underline ease-in-out duration-150">
-                Ver más
-            </a>
-        </div>
-
     </div>
 </div>
