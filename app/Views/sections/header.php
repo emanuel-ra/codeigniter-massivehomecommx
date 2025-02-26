8e77a3535e32980c9b06a85d6c20c3d3067aca9f
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2025-02-26 14:52:22 -0600

    add arial label for the search button

--- app/Views/sections/header.php
+++ app/Views/sections/header.php
@@ -54,7 +54,8 @@
 
                 <button
                     onclick="toggleModalSearching()"
-                    class="block rounded-sm bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden">
+                    class="block rounded-sm bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden"
+                    aria-label="Search">
                     <span class="sr-only">Search</span>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-search">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
